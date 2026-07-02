8328defa18737a9d6bee3e395c80fb1d607b0e1a
Sean Shmilov <sean.shmelyov@gmail.com>
2026-07-02 13:58:02 +0300

    Update title to Maxim Goat

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Players from './components/Players'
 function App() {
   return (
     <div style={{ maxWidth: 700, margin: '0 auto', padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>⚽ FifaApp</h1>
+      <h1>⚽ FifaApp Maxim Goat</h1>
       <Players />
     </div>
   )
